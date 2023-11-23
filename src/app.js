63da6ec15107561fbeff708d609ba855558c97d6
Lina María <66230572+linamaria126@users.noreply.github.com>
2023-11-23 16:19:29 +0000

    commit Random Card

--- src/app.js
+++ src/app.js
@@ -1,11 +1,52 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Variables
+
+let suits = ["♦", "♠", "♥", "♣"];
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+//Funciones
+
+let generateRandomNumber = () => {
+  let indexNumber = Math.floor(Math.random() * numbers.length);
+  console.log("index Num:" + indexNumber);
+  console.log(numbers[indexNumber]);
+  return numbers[indexNumber];
+};
+
+let generateRandomSuit = () => {
+  let indexSuit = Math.floor(Math.random() * suits.length);
+  let color = indexSuit % 2 == 0 ? "card_red" : "card_black";
+  console.log("index Suit:" + indexSuit);
+  console.log(suits[indexSuit]);
+  console.log(color);
+  return [color, suits[indexSuit]];
+};
+
+window.onload = () => {
+  let suit = generateRandomSuit();
+
+  document.querySelector(".card").classList.add(suit[0]);
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+  document.querySelector(".top-suit").innerHTML = suit[1];
+  document.querySelector(".bottom-suit").innerHTML = suit[1];
 };
